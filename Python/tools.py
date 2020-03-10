402a764ee1a3cfa1a8e48c4369ba1a24c78b2523
dmcxblue <41899653+dmcxblue@users.noreply.github.com>
2020-03-09 21:18:15 -0700

    Update tools.py

--- Python/tools.py
+++ Python/tools.py
@@ -2,6 +2,8 @@
 # Favorite tools
 # Scripted cause I have to download again everytime I start fresh
 # It's annoying
+# This tool doesnt automatically installt hem....Yet
+# Won't work on that becasue I don't want it to force install any packages
 # Version 0.1
 
 import subprocess
@@ -11,14 +13,23 @@ import os
 print ("Tool installer for Red Team")
 print ('')
 print ("Or hacking in general")
+print ("THIS WILL NOT INSTALL THE JUST DOWNLOAD THE REPO'S")
 print ("Please run as root files get saved on your '/opt' PATH")
 print ('')
 print ("Ready ?")
+
+# Extra packages so some of the tools work
+
+package1 ='https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb'
 
 ready = input('> ')
 
 if ready == 'Y' or 'y':
 	os.chdir('/opt')
+	# First we will update
+	os.system('apt update')
+	os.system('apt upgrade')
+	os.system('apt dist-upgrade')
 	subprocess.call(['git', 'clone', 'https://github.com/BC-SECURITY/Empire.git'])
 	subprocess.call(['git', 'clone', 'https://github.com/sevagas/macro_pack.git'])
 	subprocess.call(['git', 'clone', '--recursive', 'https://github.com/cobbr/Covenant.git'])
@@ -41,9 +52,12 @@ if ready == 'Y' or 'y':
 	subprocess.call(['git', 'clone', 'https://github.com/zerosum0x0/koadic.git'])
 	subprocess.call(['git', 'clone', 'https://github.com/codewhitesec/LethalHTA.git'])
 	subprocess.call(['git', 'clone', 'https://github.com/SecureAuthCorp/impacket.git'])
+	subprocess.call(['git', 'clone', 'https://github.com/byt3bl33d3r/SprayingToolkit.git'])
 	# This will install Spider requirements
 	# subprocess.call(['pip', 'install', '-r', '/opt/spiderfoot/requirements.txt')]
 	# Continue with tools
+	# Installing packages
+	os.system('wget -q ' package1)
 elif ready == 'N':
 	print ("OK")
 
